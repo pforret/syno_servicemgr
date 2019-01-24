6591a5d29afbdf38918425c7987859a4778eb327
Brightfish R&D <support@cinemapub.be>
2019-01-24 18:10:43 +0100

    beta

--- servicemgr.sh
+++ servicemgr.sh
@@ -3,42 +3,76 @@
 # author : Peter Forret <peter@forret.com>
 # inspired by http://forum.synology.com/enu/viewtopic.php?f=37&t=30242
 ##
-
 prog=$(basename $0)
-cmd=$1
+option=$1
 ### only change below
 version=1.0
-service=iperf3
-bin=/bin/iperf3
-do_start="$bin -s &"
-do_stop="kill $(pidof iperf3)"
-runas=admin
-### only chnage above
-start="bash -c"
-[[ -s "$runas" ]] && start="su - admin -c "
+svc_path=iperf3
+svc_name=$(basename "$svc_path")
+svc_port=5201
 
-case "$cmd" in
+svc_start_cmd="$svc_path -s -D"
+svc_check_proc=YES
+#svc_check_port=YES
+
+svc_stop_proc="kill -9 $(pidof $svc_name)"
+svc_stop_cmd=""
+
+svc_status_proc="pidof $svc_name"
+svc_status_port="netstat -l -p | grep $svc_port | cut -c69-"
+svc_status_cmd=
+# by default, script runs as root. Use run_as to set other user (e.g. admin)
+#runas=admin
+### only change above
+execute="bash -c"
+[[ -n "$runas" ]] && execute="su - admin -c "
+#set -ex
+
+check_port(){
+	[[ -n "$svc_port" ]] && netstat -l -p | grep $svc_port | cut -c69-
+}
+
+check_proc(){
+	pidof $svc_name
+}
+
+case "$option" in
         start)
-        $start "$do_start"
+		[[ -n "$svc_check_proc" ]] && [[ -n $(pidof $svc_name) ]] && (
+			pid=$(pidof $svc_name)
+			echo "$prog: [$svc_name] is already running (pid $pid)!"
+			ps -Af | grep $pid | grep -v grep
+			exit 1
+			)
+		echo "$prog: starting [$svc_name] ..."
+        [[ -n "$svc_start_cmd" ]] && $execute "$svc_start_cmd"
+        sleep 2
+        [[ -n "$svc_status_proc" ]] && echo "# [$svc_name] is running as port $($svc_status_proc)"
+        [[ -n "$svc_port" ]] && echo "# [$svc_name] is running on port $(netstat -l -p | grep $svc_port | cut -c69-)"
+
         ;;
 
         stop)
-        $start "$do_stop"
+		echo "$prog: stopping [$svc_name] ..."
+        [[ -n "$svc_stop_proc" ]] && $execute "$svc_stop_proc"
+        [[ -n "$svc_stop_cmd" ]] && $execute "$svc_stop_cmd"
         ;;
 
         status)
         # check for process running
         # check for port opened
+        [[ -n "$svc_status_proc" ]] && echo "# [$svc_name] is running as process $($svc_status_proc)"
+        [[ -n "$svc_port" ]] && echo "# [$svc_name] is running on port $(netstat -l -p | grep $svc_port | cut -c69-)"
+        [[ -n "$svc_status_cmd" ]]  && $svc_status_cmd
         ;;
 
         *)
 cat <<EOF
-### $prog $version
-Usage:
+### $prog $version for service [$svc_name]
 ------
-$prog start : start service $service
-$prog stop  : stop service $service
-$prog status: show status of service $service
+$prog start : start service $svc_name
+$prog stop  : stop service $svc_name
+$prog status: show status of service $svc_name
 EOF
 exit 3
 
